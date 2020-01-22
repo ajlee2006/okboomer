10ef7360c20638971ec240079a41fbd5b3e8a622
hnngggrrrr <57880110+hnngggrrrr@users.noreply.github.com>
2020-01-22 16:13:47 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-const julian = require('julian');
 
 function al() {
     document.getElementById('chn').innerHTML = document.getElementById('item1').value
 }
 function time() {
-    let now = Date.now()
 
+    let now = Date.now()
     document.getElementById('og').innerHTML = now
     document.getElementById('ogg').innerHTML = julian(now)
 
