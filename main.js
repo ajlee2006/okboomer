3c9568e8cc2b5e5bb5b951be63660865a7661122
hnngggrrrr <57880110+hnngggrrrr@users.noreply.github.com>
2020-01-22 16:56:34 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-
 function al() {
     document.getElementById('chn').innerHTML = document.getElementById('item1').value
 }
 function time() {
-
     let now = Date.now()
     document.getElementById('og').innerHTML = now
-    document.getElementById('ogg').innerHTML = julian(now)
-
+    dateObj = new Date(now);
+    d8 = dateObj.toUTCString();
+    let da = d8.split(/ +/)
+    document.getElementById('ogg').innerHTML = `${da[1]} ${da[2]} ${da[3]}`
 }
