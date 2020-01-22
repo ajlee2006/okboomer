b8cb94e117ebb5a535fdd32e2cd14d2f058beb29
hnngggrrrr <57880110+hnngggrrrr@users.noreply.github.com>
2020-01-22 15:48:02 +0800

    df

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 function al() {
     document.getElementById('chn').innerHTML = document.getElementById('item1').value
 }
+function time() {
+    document.getElementById('og').innerHTML = Date.now()
+    let now = Date.now()
+    document.getElementById('ogg').innerHTML = `${now.getDate()}/${now.getMonth()}/20`
+}
