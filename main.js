a51e0588a2e9e073998acd1f30dd595202be0088
hnngggrrrr <57880110+hnngggrrrr@users.noreply.github.com>
2020-01-22 15:59:40 +0800

    e

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
+const julian = require('julian');
+
 function al() {
     document.getElementById('chn').innerHTML = document.getElementById('item1').value
 }
 function time() {
-    document.getElementById('og').innerHTML = Date.now()
     let now = Date.now()
-    document.getElementById('ogg').innerHTML = `${now.getDate()}/${now.getMonth()}/20`
+
+    document.getElementById('og').innerHTML = now
+    document.getElementById('ogg').innerHTML = julian(now)
+
 }
