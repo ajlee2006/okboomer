c8362cb7cd7db29048cb990c9d44426d9e132ebd
hnngggrrrr <57880110+hnngggrrrr@users.noreply.github.com>
2020-01-23 22:03:29 +0800

    help

--- README.md
+++
@@ -1,2 +0,0 @@
-## This is a website for M20202 
-it is also NUSH-related audio/video player, and the other main purpose is a calendar date converter
---
+++ app.js
@@ -0,0 +1,38 @@
+function time() {
+    let now = Date.now()
+    document.getElementById('og').innerHTML = 'unix time:' + now
+    dateObj = new Date(now);
+    d8 = dateObj.toUTCString();
+    let da = d8.split(/ +/)
+    d8 = document.getElementById('ogg').innerHTML = `${da[1]} ${da[2]} ${da[3]}`
+
+} const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("open");
+    links.forEach(link => {
+        link.classList.toggle("fade");
+    });
+});
+
+function myFunction(x) {
+    x.classList.toggle("change");
+}
+
+$("a").on("click", function (event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $("html, body").animate(
+            {
+                scrollTop: $(hash).offset().top
+            },
+            800,
+            function () {
+                window.location.hash = hash;
+            }
+        );
+    }
+});
